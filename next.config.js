0ac89ecffdcc9c535df53d55d5197f2ceb976ba5
Precious Egwuenu <egwuenuprecious7@gmail.com>
2024-01-22 23:16:10 +0100

    feat/ pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          "res.cloudinary.com",
-          "lh3.googleusercontent.com",
-        ],
-      },
-}
+  images: {
+    domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
+  },
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
-
+module.exports = withPWA(nextConfig);
