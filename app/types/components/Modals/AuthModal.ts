142c37d357c3cc6a55dcbf2a54ba47f5c33b74ce
Precious Egwuenu <egwuenuprecious7@gmail.com>
2024-03-02 20:34:39 +0100

    type error

--- app/types/components/Modals/AuthModal.ts
+++ app/types/components/Modals/AuthModal.ts
@@ -3,7 +3,7 @@ import { Dispatch, SetStateAction } from "react";
 export interface AuthModalProps {
   show: boolean;
   content: string;
-  setShow: Dispatch<SetStateAction<boolean | null>>;
+  setShow: Dispatch<SetStateAction<boolean>>;
   buttonContent?: string;
   buttonAction?: () => void;
 }
